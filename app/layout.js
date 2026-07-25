aca16d529fa43af5d7bd50ff2063712e57908268
Harshit Agarwal <84agarwalharshit@gmail.com>
2026-07-25 23:32:38 +0530

    Changed little texts

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
             <p>
-              Made with 💗 by Priyanka
+              Made with 💗 by Harshit
             </p>
           </div>
         </footer>
